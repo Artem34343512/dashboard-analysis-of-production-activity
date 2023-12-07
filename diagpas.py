ed5eb7861c23e0464373001985d777ea0b8451c3
Artem34343512 <132356263+Artem34343512@users.noreply.github.com>
2023-12-07 17:15:48 +1000

    графики производственной активности

--- diagpas.py
+++ diagpas.py
@@ -1,26 +1,20 @@
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, dcc, html
 import plotly.express as px
 import pandas as pd
 
-
 df= pd.read_csv('https://raw.githubusercontent.com/Artem34343512/dashboard-analysis-of-production-activity/main/productionactivity.csv')
 
-grouped_df = df.groupby(['Дата', 'Вид работ']).sum()
+grouped_df = df.groupby(['Дата',]).sum()
 
+df_long = grouped_df.reset_index().melt(id_vars=['Дата'], value_vars=grouped_df.columns[1:],  value_name='Количество_шт')
 
-df_long = grouped_df.reset_index().melt(id_vars=['Дата'], value_vars=grouped_df.columns[1:], var_name='Вид работ', value_name='Количество_шт')
-
-
-fig = px.scatter(df_long, x='Дата', y='Количество_шт', color='Вид работ')
-
-
-fig.show()
-
-
-
+fig = px.scatter(df_long, x='Дата', y='Количество_шт')
 
 app = Dash(__name__)
+
+app.layout = html.Div([
+ dcc.Graph(figure=fig)
+])
+
 if __name__ == '__main__':
-   app.run(debug=True)
-
-
+ app.run_server(debug=True)
