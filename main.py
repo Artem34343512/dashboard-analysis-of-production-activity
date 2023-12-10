e4df6efc9a235d00aaa58cbb40a07c8c8685562d
Artem34343512 <https://github.com/Artem34343512/Analiz_proizv_activn>
2023-12-10 11:06:07 +1000

    таблица

---
+++ main.py
@@ -0,0 +1,19 @@
+from dash import Dash, html, dash_table
+import pandas as pd
+
+# Incorporate data
+
+df = pd.read_csv('https://raw.githubusercontent.com/Artem34343512/dashboard-analysis-of-production-activity/master3/productionactivity.csv')
+
+# Initialize the app
+app = Dash(__name__)
+
+# App layout
+app.layout = html.Div([
+    html.Div(children='Анализ производственной активности'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+])
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
